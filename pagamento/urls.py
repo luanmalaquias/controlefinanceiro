5b3c0580b40fb5c01515fb4e37e14cb11cadd299
luanmalaquias <luan_malaquias@outlook.com>
2023-03-19 17:06:17 -0300

    modal deletar rapido

--- pagamento/urls.py
+++ pagamento/urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('adicionar/', criar_pagamento, name="adicionarpagamento"),
+    # FIXME devedores
     path('listar-pagamentos-por-usuarios/', listarPagamentosPorUsuarios, name="listar-pagamentos-por-usuarios"),
-    path('listar-todos-os-pagamentos/', listar_pagamentos, name='listar-todos-os-pagamentos'),
     path('listar-pagamentos-por-usuarios/<str:dataParam>', listarPagamentosPorUsuarios, name="listar-pagamentos-por-usuarios-com-data"),
+    path('listar-todos-os-pagamentos/', listar_pagamentos, name='listar-todos-os-pagamentos'),
     path('adicionarpagamentorapido/<int:id>/<str:data>', criar_pagamento_rapido, name='adicionarpagamentorapido'),
     path('deletarpagamentorapido/<int:id>/<str:data>', deletar_pagamento_rapido, name='deletarpagamentorapido'),
     path('deletar-pagamento/<int:id>', deletar_pagamento, name='deletar-pagamento'),
