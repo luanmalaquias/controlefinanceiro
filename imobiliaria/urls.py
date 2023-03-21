7501b82f00caaf0f1a8d3a2e33902fdd8728d7ba
luanmalaquias <luan_malaquias@outlook.com>
2023-03-21 11:34:14 -0300

    some fixes read user

--- imobiliaria/urls.py
+++ imobiliaria/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [    
     path('cadastrar/', cadastrarImovel, name="cadastrarimovel"),
     path('listar/', listarImoveis, name="listarimoveis"),
+    # TODO read property
     path('list-available-properties/', listAvailableProperties, name='list-available-properties'),
     path('atualizar/<int:id>', atualizarDadosImovel, name="atualizardadosimovel"),
     path('deletar/<int:id>', deletarImovel, name="deletarimovel"),
